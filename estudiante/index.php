52fcfd266674a945420985468ef956fb135d5c15
srbleu <pedro.navaspe@alum.uca.es>
2021-03-29 21:50:11 +0200

    App Map

--- estudiante/index.php
+++ estudiante/index.php
@@ -1,6 +1,18 @@
 <?php
 
+session_start();
 
-echo "Logeado puto";
+if(isset($_POST['examenes'])){
+    header('location: /estudiante/examenes.php');
+}
 
+if(isset($_POST['perfil'])){
+    header('location: /estudiante/perfil.php');
+}
+
+if(isset($_POST['calificaciones'])){
+    header('location: /estudiante/calificaciones.php');
+}
 ?>
+
+
