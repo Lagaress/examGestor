c4e50c2fd3423141247796cce5b68141fb9c7680
Lagaress <jesuslagaresgalan@gmail.com>
2021-03-30 11:29:54 +0200

    Creacion de los archivos para el menu del profesor

---
+++ profesor/index.php
@@ -0,0 +1,18 @@
+<?php
+session_start() ; // Habra que pasarle el identificador de sesion
+?>
+
+<!-- Empieza el HTML -->
+<!DOCTYPE html>
+<html>
+<body>
+
+	<h1>Portal del Profesor</h1>
+	<p>¿Qué quieres hacer?</p>
+	<li><a classs = "gestionPreguntas" href = "gestionPreguntas.php">Gestionar Preguntas</a></li>
+	
+	<li><a classs = "gestionResultados" href = "gestionResultados.php">Gestionar Resultados</a></li>
+	
+
+</body>
+</html>
