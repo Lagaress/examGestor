2bcdac5a1545e9745b614dd834e3b50a05d63218
Lagaress <jesuslagaresgalan@gmail.com>
2021-03-31 19:32:27 +0200

    Detalles varios de HTML en el Index y la gestionResultados

--- profesor/gestionResultados.php
+++ profesor/gestionResultados.php
@@ -1,4 +1,13 @@
-<?php
+<!-- Empieza el HTML -->
+<!DOCTYPE html>
+<html>
+<head> 
+	<meta content = "charset=utf-8"/> 
+	<title>Ver las Calificaciones</title>
+</head>
+<body>
+	<h1>Calificaciones del profesor</h1>
+	<?php
 /* han obtenido sus estudiantes, esto es, la
 calificación que ha obtenido cada uno, el número de suspensos, de aprobados, de
 notables y de sobresalientes y finalmente la nota media de la clase*/
@@ -6,13 +15,13 @@ notables y de sobresalientes y finalmente la nota media de la clase*/
 include('./../includes/db.php'); // Incluimos la conexion a la BD
 
 // Creamos la consulta SQL para buscar las calificaciones
-$query_sql = "SELECT ALUM_DNI , 
-	NOTA , 
-	COUNT(ALUM_DNI FROM calificaciones WHERE NOTA < 5 ) , 
-	COUNT(ALUM_DNI FROM calificaciones WHERE NOTA BETWEEN 5 AND 6 ) , 
-	COUNT(ALUM_DNI FROM calificaciones WHERE NOTA BETWEEN 7 AND 8 ) , 
-	COUNT(ALUM_DNI FROM calificaciones WHERE NOTA BETWEEN 9 AND 10 ) , 
-	AVG(NOTA) , 
+$query_sql = "SELECT ALUM_DNI as DNI, 
+	NOTA as NOTA, 
+	COUNT(ALUM_DNI FROM calificaciones WHERE NOTA < 5 ) as SUSPENSOS, 
+	COUNT(ALUM_DNI FROM calificaciones WHERE NOTA BETWEEN 5 AND 6 ) as APROBADOS, 
+	COUNT(ALUM_DNI FROM calificaciones WHERE NOTA BETWEEN 7 AND 8 ) as NOTABLES, 
+	COUNT(ALUM_DNI FROM calificaciones WHERE NOTA BETWEEN 9 AND 10 ) as SOBRESALIENTES, 
+	AVG(NOTA) as MEDIA, 
 
 	FROM calificaciones c, asignaturas a , profesor p 
 
@@ -31,6 +40,45 @@ else
 {
 
 	// Ahora aquí habrá que hacer el mostrado
+	for ($i = 0 ; $i < mysql_num_rows($lista_calificaciones) ; $i++ )
+	{
+
+		$fila = mysqli_fetch_array($lista_calificaciones) ; 
+
+		if ($i == 0) // Mostramos los encabezados
+		{
+
+			echo " <table> 
+
+				<tr> 
+					<th>Estudiante</th>
+					<th>Calificación</th>
+					<th>Suspensos</th>
+					<th>Aprobados</th>
+					<th>Notables</th>
+					<th>Sobtesalientes</th>
+					<th>Media de la clase</th>
+				</tr>			
+			" ;
+
+		}
+
+		echo 
+		"
+			<tr>
+				<td>".$fila['DNI']."</td>
+				<td>".$fila['NOTA']."</td>
+				<td>".$fila['SUSPENSOS']."</td>
+				<td>".$fila['APROBADOS']."</td>
+				<td>".$fila['NOTABLES']."</td>
+				<td>".$fila['SOBRESALIENTES']."</td>
+				<td>".$fila['MEDIA']."</td>
+			</tr>
+			</table>
+		"
+	}
 
 }
 ?>
+</body>
+</html>
--- profesor/index.php
+++ profesor/index.php
@@ -1,18 +1,36 @@
-<?php
-session_start() ; // Habra que pasarle el identificador de sesion
-?>
+<?php 
+// Controlamos la sesión con PHP
+session_start();
+    $nombresesion = $_SESSION['nombre'];
+    $apellsesion = $_SESSION['apellidos'];
+    $fotosesion = $_SESSION['foto'];
+    $_SESSION['nopass']='';
+    if($nombresesion ==null || $apellsesion==null){
+        echo "no hay autorizacion";
+        die();
+    }
+
+ ?>
 
 <!-- Empieza el HTML -->
 <!DOCTYPE html>
 <html>
+<head> 
+	<meta content = "charset=utf-8"/> 
+	<title>Panel del Profesor</title>
+</head>
 <body>
 
-	<h1>Portal del Profesor</h1>
-	<p>¿Qué quieres hacer?</p>
-	<li><a classs = "gestionPreguntas" href = "gestionPreguntas.php">Gestionar Preguntas</a></li>
-	
-	<li><a classs = "gestionResultados" href = "gestionResultados.php">Gestionar Resultados</a></li>
-	
+	<h1>Operaciones del profesor</h1>
+	<table style="text-align: center ;"> 
+		<tr>
+			<th>
+				<a href="gestionResultados.php" class = "calificacionesProfesor">Ver calificaciones</a>
+			</th>
+			<th>
+				<a href="gestionPreguntas.php" class = "examenesProfesor">Gestionar Exámenes</a>
+			</th>
+		</tr>
 
 </body>
 </html>
