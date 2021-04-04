63ec73fdac9310acff7ec44febe776004039171e
Lagaress <jesuslagaresgalan@gmail.com>
2021-04-04 17:54:59 +0200

    BD con ejemplos

--- universidad.sql
+++ universidad.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 4.6.6deb5ubuntu0.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 01-04-2021 a las 15:31:04
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.3
+-- Servidor: localhost:3306
+-- Tiempo de generación: 04-04-2021 a las 11:53:56
+-- Versión del servidor: 5.7.33-0ubuntu0.18.04.1
+-- Versión de PHP: 7.2.24-0ubuntu0.18.04.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -34,6 +33,15 @@ CREATE TABLE `alumno` (
   `MATRICULADO` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`DNI`, `CURSO`, `GRADO`, `MATRICULADO`) VALUES
+('11111111B', '3', 'GII', 'PW,EDNL,POO,AAED'),
+('33432443i', '2', 'GITI', 'TERMO,FLUIDOS'),
+('45611234T', '1', 'GII', 'IP,IG');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +53,15 @@ CREATE TABLE `asignaturas` (
   `CURSOASIG` int(11) NOT NULL,
   `IDTEMAS` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `asignaturas`
+--
+
+INSERT INTO `asignaturas` (`CODIGO`, `CURSOASIG`, `IDTEMAS`) VALUES
+('EDNL', 2, '1,2,3'),
+('PW', 3, '4,5,6'),
+('AAED', 2, '6,7,8');
 
 -- --------------------------------------------------------
 
@@ -59,10 +76,18 @@ CREATE TABLE `calificaciones` (
   `NOTA` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `calificaciones`
+--
 
-INSERT INTO `calificaciones` (`CODIGO`, `ALUM_DNI`,`COD_EX`,`NOTA`) VALUES
-(1 , '33432443i', '2' , '10'),
-(2 , '33432443i', '1' , '8');
+INSERT INTO `calificaciones` (`CODIGO`, `ALUM_DNI`, `COD_EX`, `NOTA`) VALUES
+(1, '33432443i', '2', 10),
+(2, '33432443i', '1', 8),
+(10, '72638E', '3', 10),
+(20, '773956G', '3', 5),
+(200, '12345D', '3', 7),
+(300, '72638Es', '3', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -78,9 +103,14 @@ CREATE TABLE `examenes` (
   `ASIG` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `examenes` (`CODEX`, `FECHA`,`PASS`,`NUM_PREG`,`TEM`,`ASIG`) VALUES
-(1 , '2021-05-05', 'a' , '2', '2' , 'POO'),
-(2 , '2021-06-06', 'b' , '1', '2', 'EDNL');
+--
+-- Volcado de datos para la tabla `examenes`
+--
+
+INSERT INTO `examenes` (`CODEX`, `FECHA`, `PASS`, `NUM_PREG`, `TEM`, `ASIG`) VALUES
+('1', '2021-05-05', 'a', 2, '2', 'POO'),
+('2', '2021-06-06', 'b', 1, '2', 'EDNL'),
+('3', '2021-04-01', 'c', 10, 'Programacion Web', 'PW');
 
 -- --------------------------------------------------------
 
@@ -110,7 +140,7 @@ INSERT INTO `persona` (`ID`, `NOMBRE`, `APELLIDOS`, `TIPO`, `DNI`, `PASS`, `USER
 (35, 'patricio', 'abelardo', 'PROFESOR', '00000000p', 'abe', 'patri', 'default.jpg'),
 (36, 'miau', 'gatito', 'ALUMNO', '33432443i', 'miau', 'miau', 'default.jpg'),
 (38, 'admin', 'admin', 'ADMIN', 'admin2', 'admin', 'admin2', 'default.jpg'),
-(39, 'profesor', 'profesor', 'PROFESOR', '33245544r', 'profe', 'profesor', 'default.jpg'),
+(39, 'profesor', 'profesor', 'PROFESOR', '33245544r', 'prof', 'profesor', 'default.jpg'),
 (43, 'qwer', 'qwer', 'ALUMNO', 'qwer', 'qwer', 'qwer', 'default.jpg');
 
 -- --------------------------------------------------------
@@ -123,18 +153,20 @@ CREATE TABLE `preguntas` (
   `IDPREG` int(11) NOT NULL,
   `TEMAID` int(11) NOT NULL,
   `ENUNCIADO` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `RESPONSES` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `CORRECTA` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+  `RESPONSES` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CORRECTA` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `preguntas`
 --
 
--- INSERT INTO `preguntas` (`IDPREG`, `ENUNCIADO`, `RESP1`, `RESP2`, `RESP3`, `RESP4`, `RESP`) VALUES
--- (1, '¿En que año se fundo...?', '2000', '2001', '2011', '2020', 'resp1'),
--- (7, 'Esta pagina esta hecha en PHP y HTML  ', 'Verdadero   ', 'Flaso   ', '   ', '   ', 'resp1   '),
--- (8, 'como mostramos por pantalla un texto en php?', 'echo', 'balabla', 'qwer', '', 'resp1');
+INSERT INTO `preguntas` (`IDPREG`, `TEMAID`, `ENUNCIADO`, `RESPONSES`, `CORRECTA`) VALUES
+(1, 1, 'Que es un arbol', 'No es nada, no existe, no lo se, una plan', 'una plan'),
+(2, 2, 'Esto es un enunciado', 'Resp1,resp2,resp3,resp4', 'resp1'),
+(4, 4, 'Qué es PHP', 'Lo peor que se ha creado, un lenguaje, no lo se, unas siglas', 'un lenguaje'),
+(6, 5, 'Que esun framework', 'No lo se, quien sabe, wtf, una herramienta', 'una herramienta'),
+(67, 7, 'Que es una lista', 'Un suspenso, un aprobado, la compra, una ed', 'una ed');
 
 -- --------------------------------------------------------
 
@@ -144,7 +176,7 @@ CREATE TABLE `preguntas` (
 
 CREATE TABLE `profesor` (
   `DNI` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ASIGASOC` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`ASIGASOC`))
+  `ASIGASOC` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -153,15 +185,21 @@ CREATE TABLE `profesor` (
 
 INSERT INTO `profesor` (`DNI`, `ASIGASOC`) VALUES
 ('00000000p', ''),
-('33245544r', '');
+('33245544r', 'PW');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
 
 CREATE TABLE `respuestas` (
   `ID_PREG` int(11) NOT NULL,
   `ALUM_DNI` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL,
   `COD_EX` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `RESPALUMN` varchar(250) COLLATE utf8mb4_unicode_ci
+  `RESPALUMN` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -175,13 +213,23 @@ CREATE TABLE `temas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Volcado de datos para la tabla `temas`
+--
+
+INSERT INTO `temas` (`ID`, `NOMBRE`, `BATPREGUNTAS`) VALUES
+(1, 'Arboles', '1,2,3,4'),
+(2, 'Grafos', '5,6,7,8'),
+(4, 'PHP', '9,10,11'),
+(5, 'Programacion Web', '21, 321,13'),
+(8, 'Listas', '100,200,300');
+
+--
 -- Índices para tablas volcadas
 --
 
 --
 -- Indices de la tabla `alumno`
 --
-
 ALTER TABLE `alumno`
   ADD UNIQUE KEY `DNI` (`DNI`);
 
@@ -195,7 +243,7 @@ ALTER TABLE `calificaciones`
 -- Indices de la tabla `examenes`
 --
 ALTER TABLE `examenes`
-  ADD PRIMARY KEY (`CODEX`); 
+  ADD PRIMARY KEY (`CODEX`);
 
 --
 -- Indices de la tabla `persona`
@@ -231,14 +279,11 @@ ALTER TABLE `temas`
 --
 ALTER TABLE `persona`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
-
 --
 -- AUTO_INCREMENT de la tabla `preguntas`
 --
 ALTER TABLE `preguntas`
-  MODIFY `IDPREG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-COMMIT;
-
+  MODIFY `IDPREG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
